aa183f18c1b02170d6e8896252a7669f5bd513c2
brano-san <branoverv10@bk.ru>
2025-02-10 22:19:00 +0300

    Add library. Add neznayu

--- DiscountCalculationLibrary/DiscountCalculation.cs
+++ DiscountCalculationLibrary/DiscountCalculation.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IgoraDemo.Services
+﻿namespace DiscountCalculationLibrary
 {
-    internal class DiscountCalculation
+    public class DiscountCalculation
     {
         public static int calculateDiscount(int sum)
         {
--- IgoraDemo/Forms/MainForm.cs
+++ IgoraDemo/Forms/MainForm.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Windows.Forms;
 using IgoraDemo.CustomControls;
 using IgoraDemo.Forms;
-using IgoraDemo.Services;
+using DiscountCalculationLibrary;
 
 namespace IgoraDemo
 {
